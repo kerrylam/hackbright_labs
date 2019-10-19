edcebcfcacdf632ffca18f1ada1a644a1dd47afc
Your Name <you@example.com>
2019-10-19 15:38:23 -0700

    Add valid operator exception

--- calculator.py
+++ calculator.py
@@ -10,8 +10,17 @@ from arithmetic import *
 while True: 
     input_string = input()
     tokens = input_string.split(" ")
-    
+#    num1 = tokens[1]
+#    num2 = tokens[2]
+    operators = ['+', '-', '*', '/', 'square', 'cube', 'pow', 'mod']
+
     if input_string == 'q':
         break
+
+    elif tokens[0] in operators:
+        if tokens[0] == '+':
+            print(float(add(int(tokens[1]), int(tokens[2]))))
+    else:
+        print('Not a valid operator.')    
 
 # Your code goes here

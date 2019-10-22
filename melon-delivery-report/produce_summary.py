8fb644e603bbce3b5530fd95e0ae8c82a1f18f30
Kerry Lam <kqlam21@gmail.com>
2019-10-22 03:26:01 +0000

    Created function for cleaner code

--- melon-delivery-report/produce_summary.py
+++ melon-delivery-report/produce_summary.py
@@ -1,43 +1,69 @@
-print("Day 1")
-the_file = open("um-deliveries-20140519.txt")
-for line in the_file: #each line
-    line = line.rstrip() #get rid of whitespace
-    words = line.split('|') #[melon, count, amount]
+def melon_count(day_number, path):
+    """take each day's path and produce report.
 
-    melon = words[0] 
-    count = words[1]
-    amount = words[2]
+    open the report from path, process each line,
+    generate report in all caps.
+    """
+    print("Day", day_number)
+    delivery_log = open(path)
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    for line in delivery_log:
+        line = line.rstrip()
+        words = line.split('|')
 
+        melon = words[0]
+        count = words[1]
+        amount = words[2]
 
-print("Day 2")
-the_file = open("um-deliveries-20140520.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+        print("Delivered {} {}s for total of ${}".format(count, melon, amount))
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
-
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+    delivery_log.close()
 
 
-print("Day 3")
-the_file = open("um-deliveries-20140521.txt")
-for line in the_file:
-    line = line.rstrip()
-    words = line.split('|')
+melon_count(1, "um-deliveries-20140519.txt")
+melon_count(2, "um-deliveries-20140520.txt")
+melon_count(3, "um-deliveries-20140521.txt")
 
-    melon = words[0]
-    count = words[0]
-    amount = words[0]
+# print("Day 1")
+# the_file = open("um-deliveries-20140519.txt")
+# for line in the_file: #each line
+#     line = line.rstrip() #get rid of whitespace
+#     words = line.split('|') #[melon, count, amount]
 
-    print("Delivered {} {}s for total of ${}".format(
-        count, melon, amount))
-the_file.close()
+#     melon = words[0] 
+#     count = words[1]
+#     amount = words[2]
+
+#     print("Delivered {} {}s for total of ${}".format(
+#         count, melon, amount))
+# the_file.close()
+
+
+# print("Day 2")
+# the_file = open("um-deliveries-20140520.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print("Delivered {} {}s for total of ${}".format(
+#         count, melon, amount))
+# the_file.close()
+
+
+# print("Day 3")
+# the_file = open("um-deliveries-20140521.txt")
+# for line in the_file:
+#     line = line.rstrip()
+#     words = line.split('|')
+
+#     melon = words[0]
+#     count = words[0]
+#     amount = words[0]
+
+#     print("Delivered {} {}s for total of ${}".format(
+#         count, melon, amount))
+# the_file.close()

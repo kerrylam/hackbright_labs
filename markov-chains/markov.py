d275b7e790979a3dff6bb6cf2d8bac9df15aeca2
Your Name <you@example.com>
2019-11-02 16:55:08 -0700

    Added n-gram to functions

--- markov-chains/markov.py
+++ markov-chains/markov.py
@@ -1,6 +1,7 @@
 """Generate Markov text from text files."""
 
 from random import choice
+import sys
 
 
 def open_and_read_file(file_path):
@@ -15,63 +16,82 @@ def open_and_read_file(file_path):
 
     return text_string
 
-
-def make_chains(text_string):
-    """Take input text as string; return dictionary of Markov chains.
-
-    A chain will be a key that consists of a tuple of (word1, word2)
-    and the value would be a list of the word(s) that follow those two
-    words in the input text.
-
-    For example:
-
-        >>> chains = make_chains("hi there mary hi there juanita")
-
-    Each bigram (except the last) will be a key in chains:
-
-        >>> sorted(chains.keys())
-        [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
-
-    Each item in chains is a list of all possible following words:
-
-        >>> chains[('hi', 'there')]
-        ['mary', 'juanita']
-
-        >>> chains[('there','juanita')]
-        [None]
-    """
+def make_chains(text_string, n):
 
     chains = {}
-
     words = text_string.split()
     words.append(None)
     # print(words)
-
-    for i in range(len(words)-2):
-        chain_key = (words[i], words[i+1])
+    for i in range(len(words)-n):
+        chain_key = tuple(words[i:i+n])
 
         chains[chain_key] = chains.get(chain_key, [])
 
-        # if chain_key not in chains:
-        #     chains[chain_key] = []
+    # if chain_key not in chains:
+    #     chains[chain_key] = []
 
-        chains[chain_key].append(words[i+2])
-
-    # print(chains)
+        chains[chain_key].append(words[i+n])
 
     return chains
+    
 
 
-def make_text(chains):
+# def make_chains(text_string):
+#     """Take input text as string; return dictionary of Markov chains.
+
+#     A chain will be a key that consists of a tuple of (word1, word2)
+#     and the value would be a list of the word(s) that follow those two
+#     words in the input text.
+
+#     For example:
+
+#         >>> chains = make_chains("hi there mary hi there juanita")
+
+#     Each bigram (except the last) will be a key in chains:
+
+#         >>> sorted(chains.keys())
+#         [('hi', 'there'), ('mary', 'hi'), ('there', 'mary')]
+
+#     Each item in chains is a list of all possible following words:
+
+#         >>> chains[('hi', 'there')]
+#         ['mary', 'juanita']
+
+#         >>> chains[('there','juanita')]
+#         [None]
+#     """
+
+#     chains = {}
+
+#     words = text_string.split()
+#     words.append(None)
+#     # print(words)
+
+#     for i in range(len(words)-2):
+#         chain_key = (words[i], words[i+1])
+
+#         chains[chain_key] = chains.get(chain_key, [])
+
+#         # if chain_key not in chains:
+#         #     chains[chain_key] = []
+
+#         chains[chain_key].append(words[i+2])
+
+#     # print(chains)
+
+#     return chains
+
+
+def make_text(chains, n):
     """Return text from chains."""
-
+ 
     tup_key = choice(list(chains.keys()))
-    words = list(tup_key)
+    words = list(tup_key)  
     word = choice(chains[tup_key])
-
+   
     while word != None:
         words.append(word)
-        new_tup_key = (words[-2], words[-1])
+        new_tup_key = tuple(words[-n:])
         word = choice(chains[new_tup_key])
         
     # new_key = (tup_key[1], word)
@@ -80,14 +100,15 @@ def make_text(chains):
 
 
 input_path = "green-eggs.txt"
+# input_path = sys.argv[1]
 
 # Open the file and turn it into one long string
 input_text = open_and_read_file(input_path)
 
 # Get a Markov chain
-chains = make_chains(input_text)
+chains = make_chains(input_text, 3)
 
 # Produce random text
-random_text = make_text(chains)
+random_text = make_text(chains, 3)
 
 print(random_text)

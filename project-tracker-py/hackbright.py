f94e2ed54024633e1ad65ee8f080124752420ef4
kqlam21 <kqlam21@gmail.com>
2019-12-07 15:49:12 -0800

    add more functions

--- project-tracker-py/hackbright.py
+++ project-tracker-py/hackbright.py
@@ -30,9 +30,7 @@ def get_student_by_github(github):
         """
 
     db_cursor = db.session.execute(QUERY, {'github': github})
-
     row = db_cursor.fetchone()
-
     print("Student: {} {}\nGitHub account: {}".format(row[0], row[1], row[2]))
 
 
@@ -51,24 +49,39 @@ def make_new_student(first_name, last_name, github):
                                'last_name': last_name,
                                'github': github
                                })
-
     db.session.commit()
-
     print(f"Student: {first_name} {last_name}\nGitHub account: {github} added.")
+
+
+def make_new_project(title, max_grade, description):
+    """Add a new project and print confirmation.
+
+    Given a title, description, and max_grande,add project to the
+    database and print a confirmation message.
+    """
+    QUERY = """
+        INSERT INTO projects (title, max_grade, description)
+            VALUES (:title, :max_grade, :description)
+        """
+
+    db.session.execute(QUERY, {'title': title,
+                               'max_grade': max_grade,
+                               'description': description,
+                               })
+    db.session.commit()
+    print(f"Project: {title} was successfully added.")
 
 
 def get_project_by_title(title):
     """Given a project title, print information about the project."""
     QUERY = """
-        SELECT title, description, max_grade 
+        SELECT title, description, max_grade
         FROM projects
         WHERE title = :title
         """
 
     db_cursor = db.session.execute(QUERY, {'title': title})
-
     row = db_cursor.fetchone()
-
     print(f"Title: {row[0]}\nDescription: {row[1]}\nMax Grade: {row[2]}")
 
 
@@ -82,10 +95,27 @@ def get_grade_by_github_title(github, title):
         """
 
     db_cursor = db.session.execute(QUERY, {'github': github, 'title': title})
-
     row = db_cursor.fetchone()
-
     print(f"{github} received {row[0]} for project {title}.")
+
+
+def get_all_grades_for_student(first_name, last_name):
+    """Print grade for each project given student name."""
+
+    QUERY = """
+        SELECT project_title, grade
+        FROM grades
+        LEFT JOIN students
+        ON grades.student_github = students.github
+        WHERE first_name = :first_name
+        AND last_name = :last_name
+        """
+
+    db_cursor = db.session.execute(QUERY, {'first_name': first_name,
+                                           'last_name': last_name})
+    rows = db_cursor.fetchall()
+    for i in range(len(rows)):
+        print(f"{rows[i][0]}: {rows[i][1]}")
 
 
 def assign_grade(github, title, grade):
@@ -96,9 +126,7 @@ def assign_grade(github, title, grade):
         """
 
     db.session.execute(QUERY, {'github': github, 'title': title, 'grade': grade})
-
     db.session.commit()
-
     print(f"Grade assigned for project {title} to {github}.")
 
 
@@ -109,6 +137,10 @@ def handle_input():
     as a command.
     """
 
+    def handle_errors(args):
+        if args != req_args:
+            print("Invalid number of arguments, please try again!")
+
     command = None
 
     while command != "quit":
@@ -118,24 +150,47 @@ def handle_input():
         args = tokens[1:]
 
         if command == "student":
+            req_args = 1
+            handle_errors(args)
             github = args[0]
             get_student_by_github(github)
 
         elif command == "new_student":
+            req_args = 3
+            handle_errors(args)
             first_name, last_name, github = args  # unpack!
             make_new_student(first_name, last_name, github)
 
         elif command == "project":
+            req_args = 1
+            handle_errors(args)
             title = args[0]
             get_project_by_title(title)
 
         elif command == "grade":
+            req_args = 2
+            handle_errors(args)
             github, title = args
             get_grade_by_github_title(github, title)
 
         elif command == "assign_grade":
+            req_args = 3
+            handle_errors(args)
             github, title, grade = args
             assign_grade(github, title, grade)
+
+        elif command == "add_project":
+            req_args >= 3
+            handle_errors(args)
+            title, max_grade = args[:2]
+            description = (' ').join(args[2:])
+            make_new_project(title, max_grade, description)
+
+        elif command == "get_grades":
+            req_args = 2
+            handle_errors(args)
+            first_name, last_name = args
+            get_all_grades_for_student(first_name, last_name)
 
         else:
             if command != "quit":
@@ -145,7 +200,7 @@ def handle_input():
 if __name__ == "__main__":
     connect_to_db(app)
 
-    handle_input()
+    # handle_input()
 
     # To be tidy, we close our database connection -- though,
     # since this is where our program ends, we'd quit anyway.
--- project-tracker-py/hackbright.sql
+++ project-tracker-py/hackbright.sql
@@ -182,6 +182,7 @@ COPY public.projects (id, title, description, max_grade) FROM stdin;
 1	Markov	Tweets generated from Markov chains	50
 2	Blockly	Programmatic Logic Puzzle Game	10
 5	Wits and Wagers	Bidding Game	150
+6	SQL	Querying a database	80
 \.
 
 
@@ -208,7 +209,7 @@ SELECT pg_catalog.setval('public.grades_id_seq', 6, true);
 -- Name: projects_id_seq; Type: SEQUENCE SET; Schema: public; Owner: engineer
 --
 
-SELECT pg_catalog.setval('public.projects_id_seq', 5, true);
+SELECT pg_catalog.setval('public.projects_id_seq', 6, true);
 
 
 --
